a2f4124a0257a8822e4d920285d1dd6267eb719d
namirunymlih <hilminuriman109@gmail.com>
2026-01-29 14:20:12 +0700

    ganti dikit heheheh biodata_hilmy.py

--- biodata_hilmy.py
+++ biodata_hilmy.py
@@ -8,7 +8,8 @@ alamat = "Isi dengan alamat kamu"
 kota = "Isi dengan Kota kamu"
 
 print("Nama Lengkap  :", nama)
-print("Alamat        :", alamat, "\n                Kota ", kota)
+print("Alamat        :", alamat, "\n                Kota ", kota)provinsi
 print("Hobi          :", hobi)
 
 print("=======================================")
+
